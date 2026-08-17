a437bdfe3b71428e49f70d9adeb1871f04f9c533
iw4st <iw4st@users.noreply.github.com>
2026-08-17 18:00:58 +0300

    fix(webrtc): audio autoplay playback, renegotiation for screen share and camera, multiple STUN servers

--- src/config.ts
+++ src/config.ts
@@ -29,8 +29,9 @@ function parseIceServers(): IceServer[] {
     }
   }
   return [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"] },
+    { urls: ["stun:stun2.l.google.com:19302", "stun:stun3.l.google.com:19302"] },
+    { urls: "stun:global.stun.twilio.com:3478" },
   ];
 }
 
